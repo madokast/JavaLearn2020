0a53da59ebebcaa570dfa52eb85cffc2591d68b5
madokast <578562554@qq.com>
2020-01-04 11:16:22 +0800

    起飞

---
+++ springmicroservicesinaction/src/main/java/com/zrx/springmicroservicesinaction/SpringmicroservicesinactionApplication.java
@@ -0,0 +1,13 @@
+package com.zrx.springmicroservicesinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservicesinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmicroservicesinactionApplication.class, args);
+    }
+
+}
---
+++ springmicroservicesinaction/src/test/java/com/zrx/springmicroservicesinaction/SpringmicroservicesinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrx.springmicroservicesinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroservicesinactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
