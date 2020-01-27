e9fff089f984f2580c1f1a123db1aa25efee938b
madokast <578562554@qq.com>
2020-01-27 21:06:28 +0800

    u不知大

---
+++ springbootRelearn/src/main/java/com/zrx/demo/SpringbootRelearnApplication.java
@@ -0,0 +1,18 @@
+package com.zrx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主程序类
+ */
+
+@SpringBootApplication
+public class SpringbootRelearnApplication {
+
+    public static void main(String[] args) {
+        //启动
+        SpringApplication.run(SpringbootRelearnApplication.class, args);
+    }
+
+}
---
+++ springbootRelearn/src/test/java/com/zrx/demo/SpringbootRelearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRelearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
